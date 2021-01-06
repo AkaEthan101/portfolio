9f117cf8b58dcba161fb95805e8e1737bb67be27
Coderholic <Ethanhorrocks1@outlook.com>
2021-01-06 11:02:29 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    // Scroll
+
+    $(window).scroll(function() {
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    // Typing Animation
+
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "developer", "freelancer", "Discord bot programmer", "Roblox game creator"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber.", "developer.", "freelancer.", "Discord bot programmer.", "Roblox game creator."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // Toggle Navbar
+
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // Owl Carousel
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
