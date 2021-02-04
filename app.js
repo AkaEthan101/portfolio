918e28c3ce3a55c29fb926b4f94e3abc4ea1cb3a
Coderholic <Ethanhorrocks1@outlook.com>
2021-02-04 00:09:55 +0000

    .

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.get("/quitting-all-social-media", function(req, res) {
     res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 });
 
-app.listen(port);
+app.get("*", function(req, res) {
+    res.send("Page doesn't exist!");
+});
+
+app.listen(3000);
